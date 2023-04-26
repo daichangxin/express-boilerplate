a355f2dc5bb7e22bfecc58498ca00ed7b6709d57
daichangxin <daichangxin@gmail.com>
2023-04-26 09:33:35 +0800

    feat: winston logs support splice by date and size

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,15 +1,30 @@
 import { config } from '../config';
 import { createLogger, format, transports } from 'winston';
 import packageJson from '../../package.json';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+const infoTrasport = new DailyRotateFile({
+    filename: 'logs/info-%DATE%.log',
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+});
+
+const errorTransport = new DailyRotateFile({
+    level: 'error',
+    filename: 'logs/error-%DATE%.log',
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+});
 
 export const logger = createLogger({
     level: config.node_env === 'production' ? 'info' : 'debug',
     format: format.json(),
     defaultMeta: { service: packageJson.name },
-    transports: [
-        new transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new transports.File({ filename: 'logs/status.log' }),
-    ],
+    transports: [infoTrasport, errorTransport],
     exceptionHandlers: [new transports.File({ filename: 'logs/exceptions.log' })],
 });
 
